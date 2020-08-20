d5b1dd797f25e0627e2fbe494d1826a3bd0eaf1f
eziketobenna <tobeydaniels@gmail.com>
2020-08-20 12:40:38 +0100

    update dagger

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,7 +24,7 @@ object Plugin {
         const val kotlin: String = "1.4.0"
         const val androidGradle: String = "4.2.0-alpha07"
         const val navigation: String = "2.3.0"
-        const val daggerHiltAndroid: String = "2.28-alpha"
+        const val daggerHiltAndroid: String = "2.28.3-alpha"
     }
 
     const val spotless: String = "com.diffplug.spotless:spotless-plugin-gradle:${Version.spotless}"
