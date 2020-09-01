151c118bfcd29a4a092d2489b85ac904b5041457
eziketobenna <tobeydaniels@gmail.com>
2020-09-01 22:19:15 +0100

    refactor

--- character_search/src/main/java/com/ezike/tobenna/starwarssearch/character_search/model/CharacterModel.kt
+++ character_search/src/main/java/com/ezike/tobenna/starwarssearch/character_search/model/CharacterModel.kt
@@ -14,10 +14,9 @@ data class CharacterModel(
 ) : Parcelable {
     val heightInches: String?
         get() = try {
-            (
-                BigDecimal(heightCm.toDouble() * 0.393701)
-                    .setScale(1, RoundingMode.HALF_EVEN)
-                ).toString()
+            BigDecimal(heightCm.toDouble() * 0.393701)
+                .setScale(1, RoundingMode.HALF_EVEN)
+                .toString()
         } catch (e: Exception) {
             e.printStackTrace()
             null
