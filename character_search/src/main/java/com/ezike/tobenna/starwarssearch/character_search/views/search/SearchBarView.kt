79cbb2943026dd978387605a2c3f639d9146b34a
tobeydaniels@gmail.com <tobeydaniels@gmail.com>
2021-01-07 09:18:59 +0100

    complete the widgets for search history and result

--- character_search/src/main/java/com/ezike/tobenna/starwarssearch/character_search/views/search/SearchBarView.kt
+++ character_search/src/main/java/com/ezike/tobenna/starwarssearch/character_search/views/search/SearchBarView.kt
@@ -2,31 +2,41 @@ package com.ezike.tobenna.starwarssearch.character_search.views.search
 
 import android.widget.EditText
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccessTime
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AmbientConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.os.ConfigurationCompat
 import androidx.lifecycle.LifecycleCoroutineScope
 import com.ezike.tobenna.starwarssearch.character_search.R
 import com.ezike.tobenna.starwarssearch.character_search.views.rememberState
@@ -57,11 +67,102 @@ fun SearchBar() {
             SearchBar(value = value, onChange = onChange)
             Divider(
                 modifier = Modifier.padding(top = 16.dp),
-                thickness = 4.dp,
+                thickness = 3.dp,
                 color = colorResource(R.color.divider_bg)
             )
+            SearchHistory()
+            SearchResult()
         }
     }
+}
+
+@Composable
+private fun SearchResult() {
+    Box {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center)
+        )
+        LazyColumn {
+            items(List(5) {}) {
+                SearchResultItem()
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchResultItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { /*TODO*/ })
+            .padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(end = 16.dp).weight(1f),
+            text = "Luke sky walker",
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.body1
+        )
+        Icon(
+            imageVector = Icons.Outlined.KeyboardArrowRight,
+            modifier = Modifier.preferredHeight(24.dp)
+        )
+    }
+}
+
+@Composable
+private fun SearchHistoryItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { /*TODO*/ })
+            .padding(16.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.AccessTime,
+            modifier = Modifier.preferredHeight(24.dp)
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = "Luke sky walker",
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+private fun SearchHistory() {
+    Row {
+        Text(
+            modifier = Modifier.padding(16.dp).weight(1f),
+            text = stringResource(id = R.string.recent_searches).toUpperCase(
+                ConfigurationCompat.getLocales(
+                    AmbientConfiguration.current
+                )[0]
+            ),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier.clickable(onClick = { /*TODO*/ }).padding(16.dp),
+            text = stringResource(id = R.string.clear_search_history).toUpperCase(
+                ConfigurationCompat.getLocales(
+                    AmbientConfiguration.current
+                )[0]
+            )
+        )
+    }
+    Divider(
+        thickness = 2.dp,
+        color = colorResource(R.color.divider_bg)
+    )
+    Text(
+        modifier = Modifier.fillMaxWidth()
+            .padding(top = 16.dp),
+        style = TextStyle(textAlign = TextAlign.Center),
+        text = stringResource(id = R.string.search_history_empty_text)
+    )
 }
 
 @Composable
