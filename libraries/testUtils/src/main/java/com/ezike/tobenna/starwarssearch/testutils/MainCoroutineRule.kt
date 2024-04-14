f091dc9b19b0a4b5b38d27c5331a4d70388f970d
tobenna ezike <17779130+Ezike@users.noreply.github.com>
2024-04-14 13:45:31 +0200

    remove deprecated coroutine test apis

--- libraries/testUtils/src/main/java/com/ezike/tobenna/starwarssearch/testutils/MainCoroutineRule.kt
+++ libraries/testUtils/src/main/java/com/ezike/tobenna/starwarssearch/testutils/MainCoroutineRule.kt
@@ -1,28 +1,21 @@
 package com.ezike.tobenna.starwarssearch.testutils
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineExceptionHandler
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.createTestCoroutineScope
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 public class MainCoroutineRule(
-    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-) :
-    TestWatcher(),
-    TestCoroutineScope by createTestCoroutineScope(TestCoroutineDispatcher() + TestCoroutineExceptionHandler() + dispatcher) {
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
     override fun starting(description: Description) {
-        super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description) {
-        super.finished(description)
         Dispatchers.resetMain()
-        cleanupTestCoroutines()
     }
 }
